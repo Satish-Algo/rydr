eeca30e1a02971cc7a8f4e2280fd4b95cff2a4e6
Satish-Algo <satishkumarsingh2092003@gmail.com>
2026-06-08 10:45:00 +0530

    chore(eureka): update service discovery configuration and comments

--- rydr/eureka/src/main/java/com/rydr/eureka/EurekaApplication.java
+++ rydr/eureka/src/main/java/com/rydr/eureka/EurekaApplication.java
@@ -4,18 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-import com.netflix.eureka.registry.InstanceRegistry;
-import com.netflix.eureka.registry.PeerAwareInstanceRegistry;
-import com.netflix.eureka.registry.PeerAwareInstanceRegistryImpl;
 /**
- * @author oi
+ * Service Registry and Discovery Server powered by Netflix Eureka.
+ * All microservices in the Rydr ecosystem register with this server to discover peers dynamically.
+ *
+ * Default Port: 7900
+ * @author Rydr Team
  */
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaApplication {
-	
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaApplication.class, args);
-	}
-
+    
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
 }
+
