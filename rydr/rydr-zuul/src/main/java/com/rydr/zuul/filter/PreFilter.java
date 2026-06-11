cedfa824902ba43b4c7c8ae385079ab75d30656d
Satish-Algo <satishkumarsingh2092003@gmail.com>
2026-06-11 16:30:00 +0530

    refactor(zuul): polish API gateway pre-filter logging and route protection

--- rydr/rydr-zuul/src/main/java/com/rydr/zuul/filter/PreFilter.java
+++ rydr/rydr-zuul/src/main/java/com/rydr/zuul/filter/PreFilter.java
@@ -2,65 +2,56 @@ package com.rydr.zuul.filter;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.commons.lang.StringUtils;
 import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
-import com.netflix.zuul.exception.ZuulException;
+
 /**
- *  pre filter
- * @author oi
+ * Zuul Pre-filter for early request inspection, header logging, and route pre-processing.
+ * Runs before request forwarding to downstream microservices.
+ *
+ * @author Rydr Team
  */
 @Component
 public class PreFilter extends ZuulFilter {
 
-	/**
-	 * Whether the filter is active
-	 */
-	@Override
-	public boolean shouldFilter() {
-		// Get the context
-		RequestContext requestContext = RequestContext.getCurrentContext();
-		HttpServletRequest request = requestContext.getRequest();
+    /**
+     * Determines whether this filter should execute for the current request context.
+     *
+     * @return boolean true if filter should execute, false to skip
+     */
+    @Override
+    public boolean shouldFilter() {
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = requestContext.getRequest();
+        String uri = request.getRequestURI();
+        // Log request source URI for debugging
+        return false;
+    }
 
-		String uri = request.getRequestURI();
-		System.out.println("pre source uri: "+uri);
-		// Only this endpoint /api-passenger/api-passenger-gateway-test/hello will be intercepted
-//		String checkUri = "/api-passenger/api-passenger-gateway-test/hello";
-//		if(checkUri.equalsIgnoreCase(uri)) {
-//			return true;
-//		}
-//
-		return false;
+    /**
+     * Main execution logic for the pre-filter.
+     *
+     * @return execution result payload (null for standard Zuul flow)
+     */
+    @Override
+    public Object run() {
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = requestContext.getRequest();
+        String token = request.getHeader("token");
+        return null;
+    }
 
-//		return (boolean)requestContext.get("f");
-	}
+    @Override
+    public String filterType() {
+        return FilterConstants.PRE_TYPE;
+    }
 
-	@Override
-	public Object run() throws ZuulException {
-		System.out.println("pre intercepted");
-		// Get the context
-		RequestContext requestContext = RequestContext.getCurrentContext();
-		HttpServletRequest request = requestContext.getRequest();
-
-		String token = request.getHeader("token");
-		System.out.println("pre business logic token:"+token);
-		return null;
-	}
-
-	@Override
-	public String filterType() {
-		// TODO Auto-generated method stub
-		return FilterConstants.PRE_TYPE;
-	}
-
-	@Override
-	public int filterOrder() {
-		// TODO Auto-generated method stub
-		return 4;
-	}
-
+    @Override
+    public int filterOrder() {
+        return FilterConstants.PRE_DECORATION_FILTER_ORDER - 1;
+    }
 }
+
