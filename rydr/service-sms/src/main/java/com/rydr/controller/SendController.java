80dda54786e7968e593f5aef52318424b45d2588
Satish-Algo <satishkumarsingh2092003@gmail.com>
2026-06-15 13:10:00 +0530

    style(sms): format SMS delivery service and template handlers

--- rydr/service-sms/src/main/java/com/rydr/controller/SendController.java
+++ rydr/service-sms/src/main/java/com/rydr/controller/SendController.java
@@ -3,33 +3,41 @@ package com.rydr.controller;
 import com.rydr.common.dto.sms.SmsSendRequest;
 import com.rydr.service.AliService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rydr.dto.ResponseResult;
 
 import lombok.extern.slf4j.Slf4j;
 import net.sf.json.JSONObject;
+
 /**
- * @author oi
+ * Controller endpoint for handling outgoing SMS message delivery requests.
+ *
+ * @author Rydr Team
  */
 @RestController
 @RequestMapping("/send")
 @Slf4j
 public class SendController {
 
-	@Autowired
-	private AliService aliService;
+    @Autowired
+    private AliService aliService;
 
-	@RequestMapping(value = "/alisms-template",method = RequestMethod.POST)
-    public ResponseResult send(@RequestBody SmsSendRequest smsSendRequest){
-		// Output received parameter content
+    /**
+     * Send SMS notification via Aliyun SMS template service.
+     *
+     * @param smsSendRequest request containing recipient phone numbers, template code, and parameters
+     * @return ResponseResult status indicating dispatch outcome
+     */
+    @PostMapping("/alisms-template")
+    public ResponseResult send(@RequestBody SmsSendRequest smsSendRequest) {
         JSONObject param = JSONObject.fromObject(smsSendRequest);
-        log.info("/send/alisms-template   request: "+param.toString());
+        log.info("[SMS Gateway] Dispatching SMS template request: {}", param);
         aliService.sendSms(smsSendRequest);
-        return  ResponseResult.success("");
+        return ResponseResult.success("");
     }
-
 }
+
