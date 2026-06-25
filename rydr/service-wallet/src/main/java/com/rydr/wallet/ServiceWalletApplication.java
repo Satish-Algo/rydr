a901aecae4bce46a7c684336a5c664ad05058796
Satish-Algo <satishkumarsingh2092003@gmail.com>
2026-06-25 10:20:00 +0530

    refactor(wallet): enhance balance deduction validation and logging

--- rydr/service-wallet/src/main/java/com/rydr/wallet/ServiceWalletApplication.java
+++ rydr/service-wallet/src/main/java/com/rydr/wallet/ServiceWalletApplication.java
@@ -3,15 +3,20 @@ package com.rydr.wallet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 /**
- * @author oi
+ * Passenger & Driver Wallet Balance Microservice Application.
+ * Manages user wallet accounts, transaction histories, and balance updates.
+ *
+ * Default Port: 8006
+ * @author Rydr Team
  */
 @EnableEurekaClient
 @SpringBootApplication
 public class ServiceWalletApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServiceWalletApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceWalletApplication.class, args);
+    }
 }
+
